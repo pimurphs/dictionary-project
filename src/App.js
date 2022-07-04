81539ae33f440af3875888598be1557df5717bad
pimurphs <pi.rodrigues@live.com>
2022-07-04 12:02:26 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import p1 from "./p1.png";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={p1} className="App-logo img-fluid" alt="logo" />
+      </header>
+    </div>
+  );
+}
