b7e9f37f6c49ae6f248935e8dae3c12bde171f1e
pimurphs <pi.rodrigues@live.com>
2022-07-04 12:35:17 +0100

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import "./App.css";
 import p1 from "./p1.png";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={p1} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={p1} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Filipa Rodrigues</small>
+        </footer>
+      </div>
     </div>
   );
 }
